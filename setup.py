9eaebdb94c50705479a02719a765b9852d51aeaa
Vitor Veiga Vasconcelos Neto <108374481+vitorneto43@users.noreply.github.com>
2025-02-28 15:31:13 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiba",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pygments",
+        "jedi",
+        "diffusers",
+        "transformers",
+        "torch",
+        "requests",
+        "boto3",
+        "psutil",
+        "tensorflow",
+        "opencv-python",
+        "numba",
+        "llvmlite",
+        "tk"
+    ],
+    entry_points={
+        "console_scripts": [
+            "aiba=aiba_compiler:main",
+            "aiba-ide=aiba_ide:main",
+            "aiba-pkg=aiba_pkg_manager:main"
+        ]
+    },
+    author="Seu Nome",
+    description="AIBA - Linguagem de Programação para Inteligência Artificial",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/seu-usuario/aiba-lang",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
